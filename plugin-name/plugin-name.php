377c4017a1c3526dcfd85377aba5c625729dc074
Tim van Iersel <tim@timvaniersel.com>
2026-04-21 16:19:02 +0200

    Simplify plugin classes and bump version
    Remove the unused $plugin_name property and constructor parameter from admin and public classes, and replace dynamic enqueue handles with the fixed 'plugin-name' handle. Clean up demo docblocks in those classes. Also update plugin header and PLUGIN_NAME_VERSION from 1.0.0 to 0.1.0. These changes simplify class initialization and ensure consistent asset handles.

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WordPress Plugin Boilerplate
  * Plugin URI:        http://example.com/plugin-name-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           0.1.0
  * Author:            Your Name or Your Company
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
@@ -32,10 +32,9 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * Current plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'PLUGIN_NAME_VERSION', '0.1.0' );
 
 /**
  * The code that runs during plugin activation.
